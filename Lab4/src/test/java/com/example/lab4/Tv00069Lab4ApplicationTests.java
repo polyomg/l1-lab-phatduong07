47a85a8814b9b559906a42c2413ecbbb0b3ee1db
lab-3-phatduong07 <vanphat6588@gmail.com>
2025-10-04 12:01:51 +0700

    TV00069-Duong van Phat - Lab4

--- Lab4/src/test/java/com/example/lab4/Tv00069Lab4ApplicationTests.java
+++ Lab4/src/test/java/com/example/lab4/Tv00069Lab4ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.example.lab4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Tv00069Lab3ApplicationTests {
+class Tv00069Lab4ApplicationTests {
 
 	@Test
 	void contextLoads() {
